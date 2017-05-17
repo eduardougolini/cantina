d8882cd5a99c07a53759baa70146e86e182975db
Eduardo Ugolini <edu.ugolini2@gmail.com>
2017-05-17 09:38:22 -0300

    Adicionada tela de login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,18 @@
+
+<head>
+    <title>Cantina - Login</title>
+    <link href="css/login.css" rel="stylesheet" type="text/css"/>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en">
+</head>
+
+<body>
+    <form class="login">
+        <div class="userImage">
+            <img src="css/images/user.png" width="150px"/>
+        </div>
+        <input class="usermail" name="usermail" type="email" placeholder="Email" />
+        <input class="password" name="password" type="password" placeholder="Senha"/>
+        <input class="submit" name="submit" type="submit" value="Login"/>
+        <a class="forgotPassword">Esqueci minha senha</a>
+    </form>
+</body>
