5dd52b27beddbac84a325567668c46dc4f81690d
Eduardo Ugolini <edu.ugolini2@gmail.com>
2017-05-31 07:48:41 -0300

    Adicionado Polymer

--- resources/views/registerSales.blade.php
+++ resources/views/registerSales.blade.php
@@ -6,16 +6,22 @@
 @endsection
         
 @section('sidebar')
+    <script src="Polymer/bower_components/webcomponentsjs/webcomponents-lite.js" type="text/javascript"></script>
+    <link rel="import" href="Polymer/bower_components/paper-dropdown-menu/paper-dropdown-menu.html">
+    <link rel="import" href="Polymer/bower_components/paper-item/paper-item.html">
+    <link rel="import" href="Polymer/bower_components/paper-listbox/paper-listbox.html">
     @parent
 @endsection
 
 @section('content')
-<paper-dropdown-menu label="Dinosaurs">
-  <paper-listbox slot="dropdown-content" selected="1">
-    <paper-item>allosaurus</paper-item>
-    <paper-item>brontosaurus</paper-item>
-    <paper-item>carcharodontosaurus</paper-item>
-    <paper-item>diplodocus</paper-item>
-  </paper-listbox>
+
+<paper-dropdown-menu label="Cliente">
+    <paper-listbox slot="dropdown-content" selected="1">
+        <paper-item>allosaurus</paper-item>
+        <paper-item>brontosaurus</paper-item>
+        <paper-item>carcharodontosaurus</paper-item>
+        <paper-item>diplodocus</paper-item>
+    </paper-listbox>
 </paper-dropdown-menu>
+
 @endsection
