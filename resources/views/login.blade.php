38d0bcb8939b36e26c3a80e82948109631177af1
Eduardo Ugolini <edu.ugolini2@gmail.com>
2017-06-17 13:30:15 -0300

    Refeita tela de login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,18 +3,23 @@
     <title>Cantina - Login</title>
     <link href="css/login.css" rel="stylesheet" type="text/css"/>
     <script src="js/vendor/jquery-3.2.1.min.js" type="text/javascript"></script>
+    <link href="css/mdl/1.3.0/material.cyan-light_blue.min.css" rel="stylesheet" type="text/css"/>
+    <script src="js/vendor/mdl/1.3.0/material.min.js" type="text/javascript"></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en">
 </head>
 
 <body>
     <form class="login" action="/login/authenticate" method="POST">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
-        <div class="userImage">
-            <img src="css/images/user.png" width="150px"/>
+        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+            <input class="mdl-textfield__input usermail" name="usermail" type="email" />
+            <label class="mdl-textfield__label" for="sample3">Email...</label>
         </div>
-        <input class="usermail" name="usermail" type="email" placeholder="Email" />
-        <input class="password" name="password" type="password" placeholder="Senha"/>
-        <input class="submit" name="submit" type="submit" value="Login"/>
+        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+            <input class="mdl-textfield__input password" name="password" type="password" />
+            <label class="mdl-textfield__label">Senha...</label>
+        </div>
+        <input class="mdl-button mdl-js-button mdl-button--primary submit" name="submit" type="submit" value="Login"/>
         <a class="forgotPassword">Esqueci minha senha</a>
     </form>
 </body>
